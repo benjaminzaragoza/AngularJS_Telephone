bebb163c70dd930734943029ecbcec7be6b49c84
benjaminzaragoza <benjaminzaragoza@iesebre.com>
2018-10-25 11:32:17 +0200

    Initial commit

---
+++ WebApplication1/phone-list/phone-list.component.js
@@ -0,0 +1,11 @@
+﻿angular.
+  module('phoneList').
+  component('phoneList', {
+    templateUrl: 'phone-list/phone-list.html',
+    controller: ['Phone',
+      function PhoneListController(Phone) {
+        this.phones = Phone.query();
+        this.orderProp = 'age';
+      }
+    ]
+  });
